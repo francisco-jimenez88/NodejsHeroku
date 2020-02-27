0c07f01e5dc4de082baf774dbdb6cb8255456a77
NelushX <hjalle1990@hotmail.com>
2020-02-27 15:11:57 +0100

    backend

--- model/productSchema.js
+++ model/productSchema.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
 
-const schemaProduct = new mongoose.Schema(
-    {
-        name: {type: String, required: true, minlength: 1},
-        price: {Number, required: true},
-        img: { data: Buffer, contentType: String, required: true },
-        description: {String, required: true}
-    }
-)
+const schemaProduct = new mongoose.Schema({
+    category: { type: String, required: true },
+    name: { type: String, minlength: 3, required: true },
+    price: { type: Number, required: true },
+    img: { data: Buffer, contentType: String },
+    description: { type: String, required: true },
+    createdByAdmin: { type: String, required: true },
+    date: { type: Date, default: Date.now }
+});
+
 
 const newCandy = mongoose.model("List of candy", schemaProduct);
 
