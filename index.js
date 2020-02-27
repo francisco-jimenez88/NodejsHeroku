bbc4b86b957a4d5f82aa8485da1f22a5af24621d
maklet <makletlindfors@gmail.com>
2020-02-27 11:38:17 +0100

    im here

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("./config/config");
+
+const path = require("path");
+const app = express();
+
+app.use(express.urlencoded( {extended: true }));
+app.use(express.static(path.join(__dirname, "css")));
+
+app.set("view engine", "ejs");
+
+
+
+app.get("*", (req, res) => res.send("404"));
+
+const port = process.env.PORT || 8000;
+mongoose.connect(config.databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => app.listen(port, () => console.log(`Connection success on port: ${port}`)));
