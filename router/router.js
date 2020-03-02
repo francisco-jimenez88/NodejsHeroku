60bc33def71b001459ddc34f1e62fa52d5243acb
Hanna <[email address]>
2020-03-02 11:35:10 +0100

    Design på oneProduct page

--- router/router.js
+++ router/router.js
@@ -2,32 +2,35 @@ const express = require("express");
 const router = express.Router();
 const databaseCandy = require("../model/productSchema");
 
-
+// För att komma till förstasidan 
 router.route("/")
     .get(async (req, res) => {
-
         const item = await databaseCandy.find();
 
         res.render("index.ejs", {item})
     })
-
-
+    
+// Router för att komma till sidan med alla produkter
 router.route("/allproducts")
     .get(async (req, res) => { 
 
         const allCandy = await databaseCandy.find();
-        
-        
-        
         res.render("allproducts.ejs", { allCandy })
     })
 
-
+// För att komma till en specifik produkt
 router.route("/allproducts/:id")
     .get(async (req, res) => {
         console.log(req.params.id);
         const selectedCandy = await databaseCandy.findOne({name: req.params.id})
         res.render("oneproduct.ejs", {selectedCandy})
+    }) 
+
+// För att komma till mina sidor
+router.route("/mypage")
+    .get(async (req, res) => {
+        
+        res.render("myPage.ejs")
     }) 
 
 module.exports = router;
