90819f986b8aa8285dcadbe464ac19c666fa4029
[Franco] <[francisco.a.jimenez88@gmail.com]>
2020-03-06 13:32:27 +0100

    added parallax and additional small updates

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,5 +1,5 @@
 
-describe('My First Test', function() {
+describe('The first test', function() {
     it('Should load the homepage', function() {
       cy.visit('http://localhost:8000/')
     })
--- test/server.spec.js
+++ test/server.spec.js
@@ -6,29 +6,15 @@ const  server  = require('../router/router')
 
 describe('Using express', ()=> {
 
-    // vad som ska göras innan alla test körs
-    // Det är här vi ska hämta servern och initiera
-    beforeEach(()=> {
-        const  server  = require('../router/router')
-    })
-
-    // ett test
     it('Should respond to /', (done)=> {
         request(server)
             .get('/')
             .expect(200, done)
     })
 
-    // ett tredje test
     it('Should do 200 on /allproducts', (done)=> {
         request(server)
             .get('/allproducts')
             .expect(200, done)
-    })
-
-    // vad som ka göras efter alla test är färdigkörda
-    // Det är här vi ska stänga servern
-    afterEach(()=> {
-        server.close()
     })
 })
