93f5bbf325638292bb10cc5010fc34244f1cf17b
NelushX <hjalle1990@hotmail.com>
2020-03-18 13:53:21 +0100

    nothing

--- index.js
+++ index.js
@@ -16,11 +16,9 @@ app.use(express.static(path.join(__dirname, "public")));
 
 
 app.set("view engine", "ejs");
-
 app.set('views',  [path.join(__dirname, 'views'),path.join(__dirname, 'views/public')]);
 
 app.use(lassesLakritsRouter);
-
 app.use(admin);
 
 app.get("*", (req, res) => res.send("404"));
